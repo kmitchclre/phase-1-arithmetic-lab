e4d8cfe25cee9d582f78568ad1b4058a0f8c534f
Kenny Clare <kmitchellclare@gmail.com>
2022-04-23 18:29:10 -0700

    commit tree 994513de71de4b21b7799dc0bbc3f34afc9d31bb

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.round(Math.random() * 10);
+
+let num3 = 19
+let num4 = 5
+const mod = num3 % num4
+
+const max = (1, 2, 3, 4, 5, 20);
